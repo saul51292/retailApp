2e95b5d414015546fd9068fe0746c5360481142a
saul51292 <saulhamadani@me.com>
2015-01-12 03:34:02 -0500

    retailApp
    First Commit

---
+++ retailApp/FirstViewController.swift
@@ -0,0 +1,93 @@
+//
+//  FirstViewController.swift
+//  retailApp
+//
+//  Created by Saul on 1/12/15.
+//  Copyright (c) 2015 Saul. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController,BEMSimpleLineGraphDataSource,BEMSimpleLineGraphDelegate {
+    
+    @IBOutlet var simpleGraph: BEMSimpleLineGraphView!
+    @IBOutlet var salesView: UIView!
+    var screenSize = UIScreen.mainScreen().bounds
+    var tabBarBack = UIImage (named: "empty.png")
+    var tabBarColor = UIColor(red: (244/255.0), green: (244/255.0), blue: (244/255.0), alpha: 1.0)
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        addGraph()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        navBarStyling()
+        tabBarStyling()
+        
+        
+        
+    
+    }
+    
+    
+    func navBarStyling() {
+        var nav = self.navigationController?.navigationBar
+        nav?.barTintColor = salesView.backgroundColor
+        nav?.shadowImage = tabBarBack
+       nav?.translucent = false
+        nav?.setBackgroundImage(tabBarBack, forBarMetrics: UIBarMetrics.Default)
+
+        nav?.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
+        nav?.tintColor = UIColor.whiteColor()
+        
+        var moneyBarItem = UIBarButtonItem(title: "$123", style: UIBarButtonItemStyle.Plain, target: self, action: "buttonMethod")
+        
+        navigationItem.leftBarButtonItem = moneyBarItem
+        
+        var logOutItem = UIBarButtonItem(title: "Log Out", style: UIBarButtonItemStyle.Plain, target: self, action: "buttonMethod")
+        navigationItem.rightBarButtonItem = logOutItem
+
+    }
+    
+    func tabBarStyling()
+    {
+        
+        self.tabBarController?.tabBar.shadowImage = tabBarBack
+        self.tabBarController?.tabBar.backgroundImage = tabBarBack
+        self.tabBarController?.tabBar.backgroundColor = tabBarColor
+        self.tabBarController?.tabBar.tintColor = salesView.backgroundColor
+    }
+    
+    
+    func lineGraph(graph: BEMSimpleLineGraphView!, valueForPointAtIndex index: Int) -> CGFloat {
+        let data = [1.0,2.0,3.0,2.0,0.0]
+        let data2 = [2.0,0.0,2.0,3.0,5.0]
+        return CGFloat(data[index])
+    }
+    
+    func numberOfPointsInLineGraph(graph: BEMSimpleLineGraphView!) -> Int {
+        return 5
+    }
+    
+    func addGraph()
+    {
+        
+        simpleGraph.enableYAxisLabel = true
+        self.view.addSubview(simpleGraph)
+    }
+    
+    func buttonMethod() {
+        println("Yo")
+    }
+     
+}
+
---
+++ retailApp/SecondViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SecondViewController.swift
+//  retailApp
+//
+//  Created by Saul on 1/12/15.
+//  Copyright (c) 2015 Saul. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDataSource  {
+
+    @IBOutlet var tableView: UITableView!
+    var items: [String] = ["We", "Heart", "Swift"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell:UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
+        
+        cell.textLabel?.text = self.items[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath!) {
+        println("You selected cell #\(indexPath.row)!")
+    }
+
+}
+
